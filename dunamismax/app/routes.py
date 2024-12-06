9f4b894f846eeb5a13d236c70f5193da9e3da321
DunamisMax <dunamismax@tutamail.com>
2024-12-06 17:29:04 +0000

    Added Blog page

--- dunamismax/app/routes.py
+++ dunamismax/app/routes.py
@@ -6,3 +6,8 @@ bp = Blueprint("main", __name__)
 @bp.route("/")
 def home():
     return render_template("index.html", title="Dunamis Max")
+
+
+@bp.route("/blog")
+def blog():
+    return render_template("blog.html", title="Blog")

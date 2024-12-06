8fe90a611acb6a6b5e4eccc1b151fae08d81b616
DunamisMax <dunamismax@tutamail.com>
2024-12-06 18:18:03 +0000

    Updated config secret key handling

--- dunamismax/config.py
+++ dunamismax/config.py
@@ -1,8 +1,11 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "default-key")
+    SECRET_KEY = os.getenv("SECRET_KEY", "default-key-if-missing")
     DEBUG = os.getenv("FLASK_ENV") == "development"
 
 

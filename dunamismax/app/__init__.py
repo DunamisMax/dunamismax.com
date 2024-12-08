4affcfdf33872dcccd9ebc7090ef340a3f8a1b60
DunamisMax <dunamismax@tutamail.com>
2024-12-08 13:23:26 +0000

    Redirected Flask errors to Gunicorn

--- dunamismax/app/__init__.py
+++ dunamismax/app/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
+import logging
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object("config.Config")
+
+    if not app.debug:
+        gunicorn_error_logger = logging.getLogger("gunicorn.error")
+        app.logger.handlers = gunicorn_error_logger.handlers
+        app.logger.setLevel(logging.INFO)
 
     # Register blueprints or routes here
     with app.app_context():
--- dunamismax/run.py
+++ dunamismax/run.py
@@ -1,6 +1,12 @@
 from app import create_app
+import logging
 
 app = create_app()
+
+if not app.debug:
+    gunicorn_error_logger = logging.getLogger("gunicorn.error")
+    app.logger.handlers = gunicorn_error_logger.handlers
+    app.logger.setLevel(logging.INFO)
 
 if __name__ == "__main__":
     app.run(debug=True)
